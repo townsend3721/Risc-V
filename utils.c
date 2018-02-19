78575eced95c7a24cfaa8e918ab302d3ef393b2c
Townsend Saunders <townsend3721@berkeley.edu>
2018-02-19 00:44:42 -0800

    trying something

--- utils.c
+++ utils.c
@@ -66,21 +66,25 @@ int get_jump_offset(Instruction instruction) {
         unsigned int bits : 20;
     };
     struct bit_field *bitField1 = (struct bit_field *) malloc(sizeof(struct bit_field));
-    unsigned int list1[10] = {9,10,11,12,13,14,15,16,17,18};
-    unsigned int list2 [8] = {0,1,2,3,4,5,6,7};
-    int i1;
-    for (i1 = 0; i1 < 10; i1++) {
-        unsigned int j = 0;
-        bitField1->bits = set_bit(bitField1->bits, j, get_bit(instruction.ujtype.imm, list1[i1]));
-        j++;
-    }
+    bitField1->bits = set_bit(bitField1->bits, 0, get_bit(instruction.ujtype.imm, 9));
+    bitField1->bits = set_bit(bitField1->bits, 1, get_bit(instruction.ujtype.imm, 10));
+    bitField1->bits = set_bit(bitField1->bits, 2, get_bit(instruction.ujtype.imm, 11));
+    bitField1->bits = set_bit(bitField1->bits, 3, get_bit(instruction.ujtype.imm, 12));
+    bitField1->bits = set_bit(bitField1->bits, 4, get_bit(instruction.ujtype.imm, 13));
+    bitField1->bits = set_bit(bitField1->bits, 5, get_bit(instruction.ujtype.imm, 14));
+    bitField1->bits = set_bit(bitField1->bits, 6, get_bit(instruction.ujtype.imm, 15));
+    bitField1->bits = set_bit(bitField1->bits, 7, get_bit(instruction.ujtype.imm, 16));
+    bitField1->bits = set_bit(bitField1->bits, 8, get_bit(instruction.ujtype.imm, 17));
+    bitField1->bits = set_bit(bitField1->bits, 9, get_bit(instruction.ujtype.imm, 18));
     bitField1->bits = set_bit(bitField1->bits, 10, get_bit(instruction.ujtype.imm, 8));
-    int i2;
-    for (i2 = 0; i2 < 8; i2++) {
-        unsigned int j = 11;
-        bitField1->bits = set_bit(bitField1->bits, j, get_bit(instruction.ujtype.imm, list2[i2]));
-        j++;
-    }
+    bitField1->bits = set_bit(bitField1->bits, 11, get_bit(instruction.ujtype.imm, 0));
+    bitField1->bits = set_bit(bitField1->bits, 12, get_bit(instruction.ujtype.imm, 1));
+    bitField1->bits = set_bit(bitField1->bits, 13, get_bit(instruction.ujtype.imm, 2));
+    bitField1->bits = set_bit(bitField1->bits, 14, get_bit(instruction.ujtype.imm, 3));
+    bitField1->bits = set_bit(bitField1->bits, 15, get_bit(instruction.ujtype.imm, 4));
+    bitField1->bits = set_bit(bitField1->bits, 16, get_bit(instruction.ujtype.imm, 5));
+    bitField1->bits = set_bit(bitField1->bits, 17, get_bit(instruction.ujtype.imm, 6));
+    bitField1->bits = set_bit(bitField1->bits, 18, get_bit(instruction.ujtype.imm, 7));
     bitField1->bits = set_bit(bitField1->bits, 19, get_bit(instruction.ujtype.imm, 19));
     return sign_extend_number((bitField1->bits) * 2, 21);
     }
@@ -90,20 +94,18 @@ int get_store_offset(Instruction instruction) {
         unsigned int bits : 12;
     };
     struct bit_field *bitField2 = (struct bit_field *) malloc(sizeof(struct bit_field));
-    unsigned int list1[5] = {0,1,2,3,4};
-    unsigned int list2[7] = {0,1,2,3,4,5,6};
-    int i1;
-    for (i1 = 0; i1 < 5; i1++) {
-        unsigned int j = 0;
-        bitField2->bits = set_bit(bitField2->bits, j, get_bit(instruction.stype.imm5, list1[i1]));
-        j++;
-    }
-    int i2;
-    for (i2 = 0; i2 < 5; i2++) {
-        unsigned int j = 5;
-        bitField2->bits = set_bit(bitField2->bits, j, get_bit(instruction.stype.imm7, list2[i2]));
-        j++;
-    }
+    bitField2->bits = set_bit(bitField2->bits, 0, get_bit(instruction.stype.imm5, 0));
+    bitField2->bits = set_bit(bitField2->bits, 1, get_bit(instruction.stype.imm5, 1));
+    bitField2->bits = set_bit(bitField2->bits, 2, get_bit(instruction.stype.imm5, 2));
+    bitField2->bits = set_bit(bitField2->bits, 3, get_bit(instruction.stype.imm5, 3));
+    bitField2->bits = set_bit(bitField2->bits, 4, get_bit(instruction.stype.imm5, 4));
+    bitField2->bits = set_bit(bitField2->bits, 5, get_bit(instruction.stype.imm7, 0));
+    bitField2->bits = set_bit(bitField2->bits, 6, get_bit(instruction.stype.imm7, 1));
+    bitField2->bits = set_bit(bitField2->bits, 7, get_bit(instruction.stype.imm7, 2));
+    bitField2->bits = set_bit(bitField2->bits, 8, get_bit(instruction.stype.imm7, 3));
+    bitField2->bits = set_bit(bitField2->bits, 9, get_bit(instruction.stype.imm7, 4));
+    bitField2->bits = set_bit(bitField2->bits, 10, get_bit(instruction.stype.imm7, 5));
+    bitField2->bits = set_bit(bitField2->bits, 11, get_bit(instruction.stype.imm7, 6));
     return sign_extend_number((bitField2->bits), 12);
 }
 
